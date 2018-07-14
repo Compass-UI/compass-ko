0ac9a485253e908bbfd7d7e98669c380b17995b0
David Shams <david.shams@gmail.com>
2018-07-14 17:15:37 -0500

    event has a lot of information: create square where clicked

--- rscript.js
+++ rscript.js
@@ -40,7 +40,34 @@ define([
     // })
     alert($);//works
     $('body').append('<h3>RequireJS and jQuery work!</h3>');
+    $('body').append('<div>Click to get (x, y) coordinates</div>');
     console.log(document);
     console.log(document.body);
     alert(document.body); // This will excecute before console.log()
+    document.body.addEventListener('click', function(event){
+        console.log(event.clientX, event.clientY);
+        createBox(event.clientX, event.clientY);
+
+    })
 })
+
+/** Utility */
+function createBox(ex, ey) {
+    //get reference to the element
+    // var element = document.getElementById(IDI); 
+
+    //a way we can acces the X and Y coordinates
+    // var position = element.getBoundingClientRect();
+    // var x = position.left;
+    // var y = position.top;
+
+    //create, style and set the X/Y of the the div element
+    var box = document.createElement("div");
+    box.className = "box";
+    box.style.left = ex+"px";
+    box.style.top = (ey -10)+"px";
+    box.style.position = "absolute";
+
+    //Apend the element to the body
+    document.body.appendChild(box);
+}
