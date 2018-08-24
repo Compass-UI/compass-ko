ce1b4352b2db1132381d0ca0cc2cc75acc593462
David Shams <david.shams@gmail.com>
2018-08-24 05:30:20 -0500

    Caching REST calls for knockout STATE

---
+++ Caching/jscript.js
@@ -0,0 +1,54 @@
+// $(function(){
+//   $.get('https://restcountries.eu/rest/v2/all', function(data){
+//       console.log(data)
+//   })  
+// })
+
+/** Cache REST call data */
+var localCache = {
+    data: {},
+    remove: function (url) {
+        delete localCache.data[url];
+    },
+    exist: function (url) {
+        return localCache.data.hasOwnProperty(url) && localCache.data[url] !== null;
+    },
+    get: function (url) {
+        console.log('Getting in cache for url' + url);
+        return localCache.data[url];
+    },
+    set: function (url, cachedData, callback) {
+        localCache.remove(url);
+        localCache.data[url] = cachedData;
+        if ($.isFunction(callback)) callback(cachedData);
+    }
+};
+
+$(function () {
+    // var url = '/echo/jsonp/';
+    var url = 'https://restcountries.eu/rest/v2/all';
+    $('#ajaxButton').click(function (e) {
+        $.ajax({
+            url: url,
+            data: {
+                test: 'value'
+            },
+            cache: true,
+            /** The meat of caching here: */
+            beforeSend: function () {
+                if (localCache.exist(url)) {
+                    doSomething(localCache.get(url));
+                    return false;
+                }
+                return true;
+            },
+            complete: function (jqXHR, textStatus) {
+                localCache.set(url, jqXHR, doSomething);
+            }
+        });
+    });
+});
+
+function doSomething(data) {
+    console.log(data);
+}
--- Debugging/kscript.js
+++ Debugging/kscript.js
@@ -1,10 +1,21 @@
     ///<reference path="./node_modules/knockout/build/output/knockout-latest.debug.js" />
 
+    debugger;
     var viewModel = {
         fname: ko.observable('David'),
         flight_number: ko.observable(123),
         formatFlightNumber: function(flight){
             return "UA " + flight()
+        },
+        show: ko.observable(true),
+        displayMessage: ko.observable(false),
+
+        showHide: function(){
+            if(this.show()){
+                this.show(false)
+            }else{
+                this.show(true)
+            }
         }
     }
     ko.applyBindings(viewModel)
